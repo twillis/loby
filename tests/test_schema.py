3f37c88f7dd37fe29d426dbb71589acc67cf9e55
Tom Willis <tom.willis@gmail.com>
2024-05-06 19:25:14 -0400

    assert Invalid

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,5 +1,12 @@
+import pytest
+import colander
+from loby import models
+from loby.schema import make_model_schema
+
+
 def test_make_schema(dbsession):
-    from loby import models
-    from loby.schema import make_model_schema
     schema = make_model_schema(models.User)
     assert schema.children
+
+    with pytest.raises(colander.Invalid):
+        schema.deserialize({})
