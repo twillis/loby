ad770c395b5fafd93c2f2332416d4032763d4e75
Tom Willis <tom.willis@gmail.com>
2024-05-11 12:37:21 -0400

    more changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./loby/templates/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#5e60ce',
+        'secondary': '#4ea8de',
+        'accent': '#48bfe3',
+        'dark': '#1e1e1e',
+        'light': '#f4f4f5',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+    },
+  },
 }
 
